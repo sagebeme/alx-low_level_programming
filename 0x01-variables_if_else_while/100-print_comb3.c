b26632f97a8806f6679595468a0780409bfe33c6
sagebeme <eudicas5k@gmail.com>
2022-09-12 01:19:34 -0700

    Betty coding

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,17 +14,14 @@ int main(void)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 8 && digit2 == 9)
-				continue;			
-			putchar(',');
-			putchar(' ');
-		}
-	}
-
-	putchar('\n');
-
-	return (0);
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');		
+if (digit1 == 8 && digit2 == 9)
+continue;		
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
