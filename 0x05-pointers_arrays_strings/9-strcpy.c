42307729cba5cd834bd9b1c4d6a8b39a97565383
sagebeme <eudicas5k@gmail.com>
2022-09-19 12:50:29 -0700

    betty coding style fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,13 +12,13 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while (*(src + index) !='\0')
+	while (*(src + index) ! ='\0')
 	{
 		*(dest + index) = *(src + index);
 		index++;
 	}
 
-	*(dest + index) = *(src +index);
+	*(dest + index) = *(src + index);
 
 	return (dest);
 }
