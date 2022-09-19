11e898da5e5bed2545bb33b1705644ed247bb40d
sagebeme <eudicas5k@gmail.com>
2022-09-19 07:53:05 -0700

    Changing the array in the loop

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,13 @@ char *_strcpy(char *dest, const char *src)
 {
 	int index = 0;
 
-	while (src[index])
+	while (*(src + index) !='\0')
 	{
-		dest[index] = src[index];
+		*(dest + index) = *(src + index);
 		index++;
 	}
+
+	*(dest + i) = *(src +index);
 
 	return (dest);
 }
