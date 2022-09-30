db80e1a0cfcf755ca0abf2669a60dd8ce97a6ea8
sagebeme <eudicas5k@gmail.com>
2022-09-30 11:18:57 -0700

    code rearrangement

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
 * Return: returns the name.
 */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 
