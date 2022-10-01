afead344989b51b0af73bdf4b8e56a7f78a700b5
sagebeme <eudicas5k@gmail.com>
2022-09-30 17:56:15 -0700

    changing the code

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,10 +10,9 @@
  * Return: Always 0 (Success)
 */
 
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int i = 0;
-
-	printf("%s\n", argv[i]);
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
