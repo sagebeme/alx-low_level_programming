af784d9c6784b76773de3ec4b3ee6aa09b6fb658
sagebeme <eudicas5k@gmail.com>
2022-10-14 14:54:08 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual 
- * 			puzzle, followed by a new line.
+ * main - Prints "Programming is like building a multilingual
+ * puzzle, followed by a new line.
  *
  * Return: 0 after ptrinting the function
  */
 int main(void)
 {
-        puts ( "\"Programming is like building a multilingual puzzle" );
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
