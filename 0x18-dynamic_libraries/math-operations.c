74e09dd44779a6a0548c9f69175e69ae602cdaa6
sagebeme <eudicas5k@gmail.com>
2022-12-13 07:08:07 -0800

    win the Jackpot

---
+++ 0x18-dynamic_libraries/math-operations.c
@@ -0,0 +1,57 @@
+/**
+ * add - a function that adds 2 numbers
+ * @num1: the 1st argumen
+ * @num2: the 2nd argumen
+ * Return: the addition of 2 integers
+ */
+int add (int num1, int num2)
+{
+	return (num1 + num2);
+}
+
+/**
+ * sub - a function that gets the difference of 2 numbers
+ * @num1: the 1st argumn
+ * @num2: the 2nd argumn
+ * Return: the difference of 2 argumn
+ */
+
+int sub (int num1, int num2)
+{
+	return (num1 - num2);
+}
+
+/**
+ * mul - a function that multiplies 2 arguments
+ * @num1: the 1st argmn
+ * @num2: the 2nd argmn
+ * Return: the multipication of 2 argument
+ */
+
+int mul (int num1, int num2)
+{
+	return (num1 * num2);
+}
+
+/**
+ * div - a function to return the division of two numbers
+ * @num1: the first argument
+ * @num2: the second argument
+ * Return: return the division of two numbers
+ */
+
+int div(int num1, int num2)
+{
+	return (num1 / num2);
+}
+
+/**
+ * mod - return the modulus of two numbers
+ * @num1: first argument
+ * @num2: the second argument
+ * Return: return the modullus of the two numbers
+ */
+int mod (int num1, int num2)
+{
+	return (num1 % num2);
+}
