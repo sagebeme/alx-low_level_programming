710fad92406755dfaba8d95b5933b2d0624551d4
sagebeme <eudicas5k@gmail.com>
2023-04-13 20:29:06 +0300

    searching algo with logic error

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 /**
  * struct listint_s - Singly linked list
  * @n: integer stored at the node
@@ -42,5 +43,6 @@ typedef struct skiplist_s
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif /*SEARCH_ALGOS_H*/
