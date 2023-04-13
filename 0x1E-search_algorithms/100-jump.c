7b0b396a6b911067afbdcdce7b4810baf3cff1cb
sagebeme <eudicas5k@gmail.com>
2023-04-13 20:26:06 +0300

    searching algo with logic error

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+/*
+ * jump_search - searches for a value
+ *                 in a sorted array
+ *
+ * @array: pointer to the first element 
+         of the array in search
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: If value is not present in array or the array is NULL, -1
+ *         Otherwise, the first index where the value is located.
+ *
+ * Description: Prints a value every time it is compared in the array
+ *             Uses the square root of the array size as the jump step
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, jump, step;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	step = sqrt(size);
+	for (i = jump = 0; jump < size && array [jump] < value;)
+	{
+		printf("Value checked array[%ld] = [%ld]\n", i, jump);
+		i = jump;
+		jump += step;
+	}
+
+	printf("Value found between indexes [%ld] = [%ld]\n", i, jump);
+
+	jump = jump < size - 1 ? jump : size - 1;
+	for (; i < jump && array[i] < value; i++)
+		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
+
+	return (array[i] == value ? (int)i : -1);
+}
