c4906b467588b087f77255e9e0809e5111e887f7
sagebeme <eudicas5k@gmail.com>
2023-04-16 22:52:28 +0300

    prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,18 +13,18 @@
  * Description: singly linked list node sturcture
  */
 
-typedef struct listint_s
+typedef struct listint_t
 {
 	/* integer stored at the node*/
 	int n;
 	/* index of the node in the list*/
 	size_t index;
 	/* pointer to next node*/
-	struct listint_s *next;
-} listint_s;
+	struct  listint_t*next;
+} listint_t;
 
 /**
- * struct skiplist_s - Singly linked list with an express lane
+ * struct skiplist_t - Singly linked list with an express lane
  *
  * @n: Integer
  * @index: Index of the node in the list
@@ -44,5 +44,10 @@ typedef struct skiplist_s
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif /*SEARCH_ALGOS_H*/
